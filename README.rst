f55edebf0e087089823ef32eb41896df4a13e8ec
cczhu <cczhu@astro.utoronto.ca>
2018-09-14 16:22:15 -0400

    Get rid of some uneeded remote testing (only need setup.py, really).

--- README.rst
+++ README.rst
@@ -2,25 +2,10 @@
 Insight
 =======
 
-
-.. image:: https://img.shields.io/pypi/v/insight.svg
-        :target: https://pypi.python.org/pypi/insight
-
-.. image:: https://img.shields.io/travis/cczhu/insight.svg
-        :target: https://travis-ci.org/cczhu/insight
-
-.. image:: https://readthedocs.org/projects/insight/badge/?version=latest
-        :target: https://insight.readthedocs.io/en/latest/?badge=latest
-        :alt: Documentation Status
-
-
-
-
 Insight project.
 
 
 * Free software: BSD license
-* Documentation: https://insight.readthedocs.io.
 
 
 Features
--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = [ ]
 
 setup_requirements = ['pytest-runner', ]
@@ -25,8 +22,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
@@ -36,7 +31,7 @@ setup(
     description="Insight project.",
     install_requires=requirements,
     license="BSD license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='insight',
     name='insight',
